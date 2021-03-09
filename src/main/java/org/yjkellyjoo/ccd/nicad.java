3dfbace2b75c43748671fbd4ab700321f9aad963
yjkellyjoo <yejinkellyjoo@gmail.com>
2021-03-09 14:08:08 +0900

    made it succeed in building with v0.7.0 grammar
    Signed-off-by: yjkellyjoo <yejinkellyjoo@gmail.com>

---
+++ src/main/java/org/yjkellyjoo/ccd/nicad.java
@@ -0,0 +1,11 @@
+package org.yjkellyjoo.ccd;
+
+
+import org.yjkellyjoo.v070.Solidityv070Parser;
+
+public class nicad {
+    public static void runNicad(Solidityv070Parser parser){
+
+
+    }
+}
--- src/main/java/org/yjkellyjoo/solidityCCD.java
+++ src/main/java/org/yjkellyjoo/solidityCCD.java
@@ -1,3 +1,5 @@
+package org.yjkellyjoo;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
@@ -6,16 +8,20 @@ import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
 
+import org.yjkellyjoo.v070.*;
+import org.yjkellyjoo.ccd.nicad;
+
 public class solidityCCD {
 
     public static void main(String[] args){
         File file = new File("src/main/resources/test.sol");
         try {
             String code = FileUtils.readFileToString(file, "UTF-8");
-            SolidityLexer lexer = new SolidityLexer(CharStreams.fromString(code));
-            Solidity parser = new Solidity(new CommonTokenStream(lexer));
+            Solidityv070Lexer lexer = new Solidityv070Lexer(CharStreams.fromString(code));
+            Solidityv070Parser parser = new Solidityv070Parser(new CommonTokenStream(lexer));
 
-            // use parser~~!
+//             use parser~~!
+            nicad.runNicad(parser);
 
         } catch (IOException e) {
             e.printStackTrace();
